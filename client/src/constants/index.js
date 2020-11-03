ec7927bb42c6266566cd01bfe384ddb13e3f8f3f
abidex4yemi <abidex4yemi@gmail.com>
2020-11-03 02:39:16 +0100

    Initial commit

---
+++ client/src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_USER_API_URL = '/api/v1';
+export const BASE_TASK_API_URL = '/api/v1';
---
+++ tasks-service/src/constants/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+module.exports = {
+  BASE_USER_API_URL: 'http://localhost:3050/api/v1',
+};
